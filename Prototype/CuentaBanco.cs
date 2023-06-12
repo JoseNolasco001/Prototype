beedcbd429945648272e7755f62ba7199b4b7401
JoseNolasco001 <104949710+JoseNolasco001@users.noreply.github.com>
2023-06-12 10:54:52 -0600

    Initial commit

---
+++ Prototype/CuentaBanco.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proxy
+{
+    internal class CuentaBanco : Cloneable
+    {
+        private int id;
+        private int monto;
+
+        public CuentaBanco(int id,int monto)
+        {
+            this.id = id;
+            this.monto = monto; 
+        }        
+        public void setMonto(int monto)
+        {
+            this.monto= monto;  
+        }
+
+        public void Display(string nombre)
+        {
+            Console.WriteLine($"\n{nombre}\nMonto: {monto}");
+        }
+        public Cloneable Clone()
+        {
+            CuentaBanco cuenta = null;
+            try
+            {
+                cuenta = (CuentaBanco) MemberwiseClone();
+            }catch (Exception e) { }
+
+            return cuenta;
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,38 @@
+﻿using proxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CuentaBanco cuentaAhorro1 = new CuentaBanco(1,200);
+            //CuentaBanco cuentaAhorro2 = cuentaAhorro1;
+
+            CuentaBanco cuentaClonada = (CuentaBanco) cuentaAhorro1.Clone();
+
+            cuentaAhorro1.Display("cuentaAhorro1");
+            //cuentaAhorro2.Display("cuentaAhorro2");
+            cuentaClonada.Display("cuentaClonada");  
+
+            cuentaClonada.setMonto(1200);
+
+            cuentaAhorro1.Display("cuentaAhorro1");
+            cuentaClonada.Display("cuentaClonada");
+            //cuentaAhorro2.setMonto(300);
+            cuentaAhorro1.Display("cuentaAhorro1");
+            //cuentaAhorro2.Display("cuentaAhorro2");
+
+            Console.WriteLine($"\n{cuentaAhorro1 == cuentaClonada}");
+            //Console.WriteLine($"\n{cuentaAhorro1 == cuentaAhorro2}");
+
+            Console.ReadLine();
+        }
+    }
+  
+}
